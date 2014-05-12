f215be53f0e071abe821888f6f35b631af01611b
rsctiltz <rsctiltz@gmail.com>
2014-05-12 15:14:18 -0500

    Initial Commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by Ryan Tiltz on 5/12/14.
+//  Copyright (c) 2014 Ryan Tiltz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
